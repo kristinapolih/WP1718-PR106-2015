437ad726ebe9563497bf3dadf7c65638b4cdc2d1
kristinapolih <christina.polih@yahoo.com>
2018-06-10 20:13:43 +0200

    Registratrion and home page done.

---
+++ Projekat/Projekat/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Projekat.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpGet, Route("")]
+        public RedirectResult Index()
+        {
+            var requestUri = Request.RequestUri;
+            return Redirect(requestUri.AbsoluteUri + "index.html");
+        }
+    }
+}
---
+++ Projekat/Projekat/Models/Korisnik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projekat.Models
+{
+    public enum POL
+    {
+        M,
+        Z
+    }
+
+    public enum ULOGA
+    {
+        Musterija,
+        Admin,
+        Vozac
+    }
+
+    public enum VOZNJA
+    {
+        Musterija,
+        Admin,
+        Vozac
+    }
+
+    public class Korisnik
+    {
+        private string korisnickoIme;
+        private string loznka;
+        private string ime;
+        private string prezime;
+        private POL pol;
+        private string jMBG;
+        private string telefon;
+        private string email;
+        private ULOGA uloga;
+        private VOZNJA voznja;
+    }
+}
